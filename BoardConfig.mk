bf0d7e09f69a9b89a7b33ef8d26a2c9e96581ffb
Kyros70 <zaedmasri000999@email.com>
2026-07-22 21:28:10 +0300

    small fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X6816D
+DEVICE_PATH := device/infinix/X6816D
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
