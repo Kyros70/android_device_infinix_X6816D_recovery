57ae616e1ecbee0e5a59e2c1d6fabda7eb7a50b7
Kyros70 <zaedmasri000999@gmail.com>
2026-07-24 06:26:46 +0300

    fixup

--- recovery/root/system/bin/load_modules.sh
+++ recovery/root/system/bin/load_modules.sh
@@ -1,21 +1,17 @@
 #!/sbin/sh
 
+# TOUCH FIX
 load_panel()
 {
+	insmod /lib/modules/gslX680.ko
 	insmod /lib/modules/adaptive-ts.ko
 	insmod /lib/modules/gpio.ko
 	insmod /lib/modules/gpio-sprd.ko
-	insmod /lib/modules/focaltech_fp.ko
-	insmod /lib/modules/sfp_core.ko
-	insmod /lib/modules/ssd20xx_ts.ko
-	insmod /lib/modules/microarray_fp.ko
+	insmod /lib/modules/focaltech_ft8756_spi_ts.ko
+	insmod /lib/modules/nvt_nt36xxx_spi_ts.ko
+	insmod /lib/modules/fortsense_fp.ko
 	insmod /lib/modules/sc27xx_tsensor_thermal.ko
 	insmod /lib/modules/tran_gesture.ko
-        insmod /lib/modules/sprd-sc27xx-spi.ko
-	insmod /lib/modules/spi-sprd.ko
-	insmod /lib/modules/core.ko
-	insmod /lib/modules/sc27xx-vibra.ko
-	insmod /lib/modules/sc27xx_adc.ko
 }
 
 
